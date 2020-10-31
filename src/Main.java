d0f828502c058b74ef8ac811a7666b64f2fe6745
AnastasiaIQA12 <Sinka8900@mail.ru>
2020-10-31 10:33:14 +0300

    Add project

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+       int account = 2_000_000_000;
+       int amount_transfer = 500_000_000;
+       int balance = account + amount_transfer;
+       System.out.println(balance);
+    }
+}
